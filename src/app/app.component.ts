23cfa5868a5fa2501e6234f7305b8e01479de874
Daniel <jmzwcn@gmail.com>
2019-09-11 21:41:59 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleBlackTranslucent();
+      this.statusBar.backgroundColorByHexString("#33000000");
       this.splashScreen.hide();
     });
   }
