cc8f683ccef939ab4c13ac24196ad4ed081459ab
Daniel <jmzwcn@gmail.com>
2019-09-16 22:46:39 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -11,6 +11,7 @@ export class Tab2Page {
     speed: 400,
     direction: "vertical",
     slidesPerView: 10,
+    height: 400,
   };
 
   items = [];
@@ -20,10 +21,15 @@ export class Tab2Page {
     this.items[1] = "水果";
     this.items[2] = "蔬菜";
     this.items[3] = "禽畜蛋肉";
-    this.items[4] = "水产";
-    for (let i = 5; i < 20; i++) {
-      this.items[i] = "menu" + i;
-    }
+    this.items[4] = "茶叶";
+    this.items[5] = "水产";
+    this.items[6] = "中药材";
+    this.items[7] = "坚果干果";
+    this.items[8] = "农副/副食";
+    this.items[9] = "粮油作物";
+    this.items[10] = "食用菌";
+    this.items[11] = "特种种植";
+    this.items[12] = "绿化苗木";
   }
 
   itemClick(item) {
