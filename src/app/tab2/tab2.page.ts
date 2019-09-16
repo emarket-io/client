6032a8d51c8c6df7e0702f7bb83a098ffd776fdb
Daniel <jmzwcn@gmail.com>
2019-09-16 22:02:08 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,8 +10,23 @@ export class Tab2Page {
     initialSlide: 0,
     speed: 400,
     direction: "vertical",
-    slidesPerView: 5,
+    slidesPerView: 10,
   };
 
-  constructor() { }
+  items = [];
+  currentItem = '热门分类';
+  constructor() {
+    this.items[0] = "热门分类";
+    this.items[1] = "水果";
+    this.items[2] = "蔬菜";
+    this.items[3] = "禽畜蛋肉";
+    this.items[4] = "水产";
+    for (let i = 5; i < 20; i++) {
+      this.items[i] = "menu" + i;
+    }
+  }
+
+  itemClick(item) {
+    this.currentItem = item;
+  }
 }
