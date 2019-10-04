f081b43791866ab45f1b8f23118c7e2bf2165d1b
Daniel <jmzwcn@gmail.com>
2019-10-04 10:35:34 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/provider/api.service.ts
+++ src/app/provider/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
-declare let AMap;
+//declare let AMap;
 
 @Injectable({
   providedIn: 'root'
@@ -11,36 +11,6 @@ export class ApiService {
   injector: Injector;
   key = '';
   currentAddress = '湖北荆门市';
-
-
-
-
- getCurrentAddress():any {
-    this.injector.get(Geolocation).getCurrentPosition().then((resp) => {
-      AMap.service('AMap.Geocoder', () => {
-        AMap.convertFrom(resp.coords.longitude + "," + resp.coords.latitude, "gps",
-          (status, result) => {
-            if (status == "complete") {
-              const positionInfo = [result.locations[0].P + '', result.locations[0].O + ''];
-              const geocoder = new AMap.Geocoder({});
-              geocoder.getAddress(positionInfo, (status, result) => {
-                if (status === 'complete' && result.info === 'OK') {
-                  // apiService.currentAddress = result.regeocode.formattedAddress;
-                  this.currentAddress = apiService.currentAddress;
-                  return result.regeocode.formattedAddress;
-                } else {
-                  alert('获取地址失败:' + status);
-                }
-              });
-            } else {
-              alert("坐标转换失败," + status + "/" + result);
-            }
-          });
-      });
-    }).catch((error) => {
-      alert('Error getting location:' + error);
-    });
-  }
 }
 
 export const apiService = new ApiService();
