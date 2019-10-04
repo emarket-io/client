c2e1becc55aad20a75d99d251225716d725f30dd
Daniel <jmzwcn@gmail.com>
2019-10-04 12:06:37 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef } from '@angular/core';
 import { apiService } from '../provider/api.service'
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
@@ -10,11 +10,33 @@ declare let AMap;
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  @ViewChild('map_container',null) map_container: ElementRef;
+  map: any; // 地图对象
 
   address = apiService.currentAddress;
   constructor(private geolocation: Geolocation) { }
 
   ionViewWillEnter() {
+    this.map = new AMap.Map(this.map_container.nativeElement, {
+      //resizeEnable: true,
+      //rotateEnable: true,
+      //pitchEnable: true,
+      zoom: 10,
+      //pitch: 80,
+      //rotation: -15,
+      viewMode: '2D',//开启3D视图,默认为关闭
+      buildingAnimation: true,//楼块出现是否带动画
+      showBuildingBlock: true,
+      expandZoomRange: true,
+      //zooms: [3, 20],
+      //center: [116.333926, 39.997245]
+      mapStyle: 'amap://styles/light',
+    });
+    AMap.plugin('AMap.ToolBar', () => {
+      var toolbar = new AMap.ToolBar();
+      this.map.addControl(toolbar);
+    });
+    this.getLocation2();
     this.getLocation();
   }
 
@@ -41,6 +63,40 @@ export class HomePage {
       });
     }).catch((error) => {
       alert('Error getting location:' + error);
+    });
+  }
+
+  getLocation2() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      AMap.service('AMap.Geocoder', () => {
+        AMap.convertFrom(resp.coords.longitude + "," + resp.coords.latitude, "gps",
+          (status, result) => {
+            if (status == "complete") {
+              const positionInfo = [result.locations[0].P + '', result.locations[0].O + ''];
+              this.map.setCenter(positionInfo);
+
+              const geocoder = new AMap.Geocoder({});
+              geocoder.getAddress(positionInfo, (status, result) => {
+                if (status === 'complete' && result.info === 'OK') {
+                  const marker = new AMap.Marker({
+                    map: this.map,
+                    position: positionInfo
+                  });
+                  marker.setLabel({
+                    offset: new AMap.Pixel(20, 20), // 修改label相对于marker的位置
+                    content: result.regeocode.formattedAddress
+                  });
+                } else {
+                  console.log('获取地址失败');
+                }
+              });
+            } else {
+              alert("坐标转换失败," + status + "/" + result);
+            }
+          });
+      });
+    }).catch((error) => {
+      console.log('Error getting location', error);
     });
   }
 }
