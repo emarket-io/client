6e5ba1e3eb329d4e27afa54424759c211d5071bb
Daniel <jmzwcn@gmail.com>
2019-10-15 21:55:48 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/seller/publish/publish.page.ts
+++ src/app/seller/publish/publish.page.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
-import { apiService } from '../../providers/api.service'
 import { Component, OnInit } from '@angular/core';
+import { apiService } from '../../providers/api.service'
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-publish',
@@ -8,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publish.page.scss'],
 })
 export class PublishPage {
-  
+
   address = apiService.currentAddress;
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private camera: Camera) { }
 
   gotoPublish() {
     this.router.navigateByUrl('/store/publish');
+  }
+
+  addMedia() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      alert(base64Image);
+    }, (err) => {
+      // Handle error
+    });
   }
 }
