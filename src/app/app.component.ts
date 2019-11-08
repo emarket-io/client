158b3417e2ae1c0776ce73b3fdb047b156240233
Daniel <jmzwcn@gmail.com>
2019-11-09 00:00:46 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,28 @@ export class AppComponent {
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {      
+    this.platform.ready().then(() => {
       this.statusBar.overlaysWebView(true);
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-
+      this.setTheme(Math.random());
       utilsService.injector = this.injector;
     });
+  }
+
+  theme = { mycolor: '', mytextcolor: '' };
+
+  setTheme(random: number) {
+    if (random <= 0.3) {
+      this.theme = { mycolor: 'rebeccapurple', mytextcolor: '#fff' };
+    } else if (0.3 < random && random <= 0.6) {
+      this.theme = { mycolor: '#f36033', mytextcolor: '#fff' };
+    } else {
+      this.theme = { mycolor: '#10dc60', mytextcolor: '#222428' };
+    }
+
+    Object.keys(this.theme).forEach(k =>
+      document.documentElement.style.setProperty(`--${k}`, this.theme[k])
+    );
   }
 }
