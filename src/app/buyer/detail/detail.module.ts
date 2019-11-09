a95c56a146c8549493bca56503cf5a53a2530221
Daniel <jmzwcn@gmail.com>
2019-11-09 17:20:04 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/buyer/detail/detail.module.ts
+++ src/app/buyer/detail/detail.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { Wechat } from '@ionic-native/wechat/ngx';
 import { IonicModule } from '@ionic/angular';
 
 import { DetailPage } from './detail.page';
@@ -21,6 +21,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
+  providers: [Wechat],
   declarations: [DetailPage]
 })
-export class DetailPageModule {}
+export class DetailPageModule { }
