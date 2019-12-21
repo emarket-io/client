310ce87536a030af9f47c78c66b16fa9f955682c
Daniel <jmzwcn@gmail.com>
2019-12-21 17:12:14 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,7 @@ const routes: Routes = [
   { path: 'message', loadChildren: './user/message/message.module#MessagePageModule' },
   { path: 'shop', loadChildren: './seller/shop/shop.module#ShopPageModule' },
   { path: 'seller_account', loadChildren: './seller/account/account.module#AccountPageModule' },
+  { path: 'setting', loadChildren: './user/setting/setting.module#SettingPageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
