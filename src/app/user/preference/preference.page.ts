aae9aa0fabfaf6eb3dc303d105c44d2167451632
Daniel <jmzwcn@gmail.com>
2019-12-28 23:41:30 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/user/preference/preference.page.ts
+++ src/app/user/preference/preference.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Events } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { utilsService } from '../../providers/utils.service'
 
 @Component({
   selector: 'app-preference',
   templateUrl: './preference.page.html',
   styleUrls: ['./preference.page.scss'],
 })
-export class PreferencePage implements OnInit {
+export class PreferencePage {
 
-  constructor() { }
+  constructor(private events: Events, private router: Router, ) { }
 
-  ngOnInit() {
+  logout() {
+    utilsService.confirm('确定要退出登录？', () => {
+      utilsService.setUser(null);
+      this.events.publish('user:logout', '');
+      this.router.navigateByUrl('/login');
+    });
   }
-
 }
