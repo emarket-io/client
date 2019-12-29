1613c4d9f1a8eb892504ad8972b8c4694a08717b
Daniel <jmzwcn@gmail.com>
2019-12-29 18:27:41 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/user/help/help.page.ts
+++ src/app/user/help/help.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.page.html',
   styleUrls: ['./help.page.scss'],
 })
-export class HelpPage implements OnInit {
+export class HelpPage {
+  detailMap = new Map<string, boolean>();
 
   constructor() { }
 
-  ngOnInit() {
+  detail(key: string) {
+    if (!this.detailMap[key]) {
+      this.detailMap[key] = false;
+    }
+    this.detailMap[key] = !this.detailMap[key];
   }
 
 }
