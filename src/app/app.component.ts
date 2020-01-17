a2b8fd8886096a202b2aa923854f0884553a9521
Daniel <jmzwcn@gmail.com>
2020-01-17 16:47:18 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,18 +78,18 @@ export class AppComponent {
   }
 
   handleUpdate() {
-    let apkSite = 'https://github.com/emart-io/client/raw/master/platforms/android/app/build/outputs/apk/release';
+    //let apkSite = 'https://github.com/emart-io/client/raw/master/platforms/android/app/build/outputs/apk/release';
     this.appVersion.getVersionNumber().then(value => {
-      alert('appVersion:' + value);
-      this.http.get(apkSite + `/output.json`).subscribe(data => {
-        alert(JSON.stringify(data));
-        alert(data[0].apkInfo.versionName);
+      //alert('appVersion:' + value);
+      this.http.get('https://raw.githubusercontent.com/emart-io/client/master/platforms/android/app/build/outputs/apk/release/output.json').subscribe(data => {
+       // alert(JSON.stringify(data));
+       // alert(data[0].apkInfo.versionName);
         if (data[0].apkInfo.versionName != value) {
           utilsService.show('发现新版本[' + data[0].apkInfo.versionName + ']，开始自动下载...');
           const fileTransfer: FileTransferObject = this.transfer.create();
           let saveurl = this.file.externalDataDirectory ? this.file.externalDataDirectory : this.file.dataDirectory;
           let apk = saveurl + 'download/' + 'daji.apk';
-          const url = apkSite + '/app-release.apk';
+          const url = 'https://github.com/emart-io/client/raw/master/platforms/android/app/build/outputs/apk/release/app-release.apk';
 
           fileTransfer.download(url, apk, true).then((entry) => {
             this.fileOpener.open(entry.toURL(),
